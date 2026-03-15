94ce60600dd9eb366be8607af53fb1399cc07197
saekoy <saeconomail@gmail.com>
2026-03-16 05:24:33 +0900

    学習ログ詳細ページを作成

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -4,12 +4,13 @@ describe 'Post', type: :system do
   before do
     driven_by :rack_test
     @user = create(:user) # ログイン用ユーザー作成
+    @post = create(:post, title: 'RSpec学習完了', content: 'System Specを作成した', user_id: @user.id) # 追加
   end
  
   # 投稿フォーム
   let(:title) { 'テストタイトル' }
   let(:content) { 'テスト本文' }
- 
+
   describe 'ログ投稿機能の検証' do
     # ログ投稿を行う一連の操作を subject にまとめる
     subject do
@@ -55,6 +56,16 @@ describe 'Post', type: :system do
           expect(page).to have_field('post_content', with: content)
         end
       end
+    end
+  end
+
+  describe 'ログ詳細機能の検証' do
+    before { visit "/posts/#{@post.id}" }
+
+    it 'Postの詳細が表示される' do
+      expect(page).to have_content('RSpec学習完了')
+      expect(page).to have_content('System Specを作成した')
+      expect(page).to have_content(@user.nickname)
     end
   end
 end
