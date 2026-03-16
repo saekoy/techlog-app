6f6911374349caa115468fb96a2f14381be412cd
saekoy <saeconomail@gmail.com>
2026-03-16 16:32:28 +0900

    rubocopの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,14 +5,18 @@ class PostsController < ApplicationController
     @posts = Post.limit(10).order(created_at: :desc)
   end
 
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
   def new
     @post = Post.new # 新規投稿用のインスタンス変数を用意
   end
- 
+
   def create
     @post = Post.new(post_params) # ストロングパラメータを使ってフォームから受け取ったパラメータを許可
     @post.user_id = current_user.id # ログインユーザのIDを代入して関連付け
- 
+
     if @post.save
       flash[:notice] = '投稿しました' # 成功時のフラッシュメッセージ
       redirect_to posts_path # トップページから投稿一覧ページへ変更
@@ -20,10 +24,6 @@ class PostsController < ApplicationController
       flash[:alert] = '投稿に失敗しました' # 失敗時のフラッシュメッセージ
       render :new # 投稿画面を再表示
     end
-  end
-
-  def show
-      @post = Post.find_by(id: params[:id])
   end
 
   def destroy
--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
- 
+
 describe 'Post', type: :system do
   before do
     driven_by :rack_test
     @user = create(:user) # ログイン用ユーザー作成
     @post = create(:post, title: 'RSpec学習完了', content: 'System Specを作成した', user_id: @user.id) # 追加
   end
- 
+
   # 投稿フォーム
   let(:title) { 'テストタイトル' }
   let(:content) { 'テスト本文' }
@@ -18,7 +18,7 @@ describe 'Post', type: :system do
       fill_in 'post_content', with: content
       click_button 'ログを記録'
     end
- 
+
     context 'ログインしていない場合' do
       before { visit '/posts/new' }
       it 'ログインページへリダイレクトする' do
@@ -26,7 +26,7 @@ describe 'Post', type: :system do
         expect(page).to have_content('ログインしてください。')
       end
     end
- 
+
     context 'ログインしている場合' do
       before do
         sign_in @user
@@ -35,7 +35,7 @@ describe 'Post', type: :system do
       it 'ログインページへリダイレクトしない' do
         expect(current_path).not_to eq('/users/sign_in')
       end
- 
+
       context 'パラメータが正常な場合' do
         it 'Postを作成できる' do
           expect { subject }.to change(Post, :count).by(1)
@@ -43,14 +43,14 @@ describe 'Post', type: :system do
           expect(page).to have_content('投稿しました')
         end
       end
- 
+
       context 'パラメータが異常な場合' do
         let(:title) { nil }
         it 'Postを作成できない' do
           expect { subject }.not_to change(Post, :count)
           expect(page).to have_content('投稿に失敗しました')
         end
- 
+
         it '入力していた内容は維持される' do
           subject
           expect(page).to have_field('post_content', with: content)
